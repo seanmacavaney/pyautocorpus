1fcd550898b92b35db37c1b96cf7c2c1f3e76014
Sean MacAvaney <sean.macavaney@gmail.com>
2021-07-06 13:49:39 +0100

    .

--- setup.py
+++ setup.py
@@ -7,6 +7,24 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+windows = sys.platform.startswith("win")
+
+include_dirs = [
+    os.path.realpath(os.path.join(__file__, '..', 'AutoCorpus', 'src', 'common')),
+    os.path.realpath(os.path.join(__file__, '..', 'AutoCorpus', 'src', 'wikipedia')),
+]
+lib_dirs = []
+libs = ['pcre']
+comp = []
+
+if windows:
+    PCRE_HOME = os.environ.get("PCRE_HOME")
+    print('PCRE_HOME', PCRE_HOME)
+    include_dirs.append(PCRE_HOME)
+    lib_dirs.append(PCRE_HOME)
+else:
+    comp.append('-std=c++11')
+
 setuptools.setup(
     name="pyautocorpus",
     version="0.0.1",
@@ -17,18 +35,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/seanmacavaney/pyautocorpus",
     include_package_data = True,
-    packages=setuptools.find_packages(include=['zlib_state']),
+    packages=setuptools.find_packages(include=['pyautocorpus']),
     install_requires=[],
     classifiers=[
         'License :: OSI Approved :: MIT License',
     ],
     python_requires='>=3.6',
     ext_modules=[setuptools.Extension("pyautocorpus", [
-            "src/pyautocorpus.cpp",
-            "AutoCorpus/src/common/utilities.cpp",
-            "AutoCorpus/src/wikipedia/Textifier.cpp",
-        ], include_dirs=[
-        os.path.realpath(os.path.join(__file__, '..', 'AutoCorpus', 'src', 'common')),
-        os.path.realpath(os.path.join(__file__, '..', 'AutoCorpus', 'src', 'wikipedia')),
-    ], libraries=['pcre'], extra_compile_args=['-std=c++11'])],
+        "src/pyautocorpus.cpp",
+        "AutoCorpus/src/common/utilities.cpp",
+        "AutoCorpus/src/wikipedia/Textifier.cpp",
+    ], include_dirs=include_dirs, libraries=libs, extra_compile_args=comp)],
 )
