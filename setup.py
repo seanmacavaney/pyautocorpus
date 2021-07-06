1a8f95e2c2574edf689538dface9e07b3c9a7a3b
Sean MacAvaney <sean.macavaney@gmail.com>
2021-07-06 15:10:19 +0100

    .

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if windows:
     include_dirs.append(PCRE_HOME)
     lib_dirs.append(PCRE_HOME)
     macros.append(('CLOCK_MONOTONIC', None))
-    macros.append(('clock_gettime', '(a,b) {} while (0)'))
+    macros.append(('clock_gettime', 'do {} while (0) //'))
 else:
     comp.append('-std=c++11')
 
