2f6432ff3b23bea76bb7c5b4398dd66ded042ee9
Sean MacAvaney <sean.macavaney@gmail.com>
2021-07-12 17:14:39 +0100

    new version with a placeholder Windows version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setuptools.setup(
     name="pyautocorpus",
-    version="0.1.0",
+    version="0.1.1",
     author="Sean MacAvaney",
     author_email="sean.macavaney@gmail.com",
     description="",
