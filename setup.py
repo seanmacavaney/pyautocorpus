86a795adf7933cc05554a1d2bd0eb6d56b144e9a
Sean MacAvaney <sean.macavaney@gmail.com>
2021-08-07 16:39:40 -0400

    can't seem to get centos build to work; reverting to just sdist
    closes #2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setuptools.setup(
     name="pyautocorpus",
-    version="0.1.5",
+    version="0.1.6",
     author="Sean MacAvaney",
     author_email="sean.macavaney@gmail.com",
     description="",
