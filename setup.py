a72245defe654c1c635228055e06261d96a6109e
Sean MacAvaney <sean.macavaney@gmail.com>
2021-12-06 09:07:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setuptools.setup(
     name="pyautocorpus",
-    version="0.1.6",
+    version="0.1.8",
     author="Sean MacAvaney",
     author_email="sean.macavaney@gmail.com",
     description="",
