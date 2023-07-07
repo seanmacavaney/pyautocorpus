25cba7bd80a99e6bb6b6596ce09d468ba63479db
Sean MacAvaney <sean.macavaney@gmail.com>
2023-07-07 09:59:51 +0100

    bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setuptools.setup(
     name="pyautocorpus",
-    version="0.1.10",
+    version="0.1.11",
     author="Sean MacAvaney",
     author_email="sean.macavaney@gmail.com",
     description="",
